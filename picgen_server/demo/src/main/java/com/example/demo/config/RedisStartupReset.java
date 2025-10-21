aa2e0f8f2b9457d61d824474e123dfc56ef83106
howiechan123 <113634531+howiechan123@users.noreply.github.com>
2025-10-21 09:05:14 -0400

    reset redis on start

--- picgen_server/demo/src/main/java/com/example/demo/config/RedisStartupReset.java
+++ picgen_server/demo/src/main/java/com/example/demo/config/RedisStartupReset.java
@@ -17,8 +17,8 @@ public class RedisStartupReset {
     @PostConstruct
     public void clearRedisOnStartup() {
         try {
-            redisTemplate.delete("queue_count");
-            redisTemplate.opsForValue().set("queue_count", "0");
+            redisTemplate.delete("hf_queue_count");
+            redisTemplate.opsForValue().set("hf_queue_count", "0");
             System.out.println("Redis reset on startup: queue_count set to 0");
         } catch (Exception e) {
             System.err.println("Failed to clear Redis on startup: " + e.getMessage());
